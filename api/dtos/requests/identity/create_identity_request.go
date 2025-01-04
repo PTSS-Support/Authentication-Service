750318bba95834265ad748089679ec5791531558
tayamEJ <Tayam0nline@gmail.com>
2025-01-04 23:04:24 +0100

    bugfix groupid

--- api/dtos/requests/identity/create_identity_request.go
+++ api/dtos/requests/identity/create_identity_request.go
@@ -8,7 +8,7 @@ type CreateIdentityRequest struct {
 	Email     string     `json:"email" binding:"required,email,min=6,max=254"`
 	Password  string     `json:"password" binding:"required,min=8,max=128"`
 	Role      enums.Role `json:"role" binding:"required,oneof=admin family_member primary_relative patient healthcare_professional"`
-	GroupID   string     `json:"groupId" binding:"uuid"`
+	GroupID   string     `json:"groupId" binding:"omitempty,uuid"`
 	FirstName string     `json:"firstName" binding:"required"`
 	LastName  string     `json:"lastName" binding:"required"`
 }
