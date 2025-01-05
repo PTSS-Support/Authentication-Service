b05577796a029b33fc6e0f144d88fbcb2032d1b8
tayamEJ <Tayam0nline@gmail.com>
2025-01-05 13:24:21 +0100

    hotfix

--- core/services/identity_service.go
+++ core/services/identity_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	requests "github.com/PTSS-Support/identity-service/api/dtos/requests/identity"
 	responses "github.com/PTSS-Support/identity-service/api/dtos/responses/identity"
@@ -39,7 +40,7 @@ func NewIdentityService(identityRepo repositories.IdentityRepository) IdentitySe
 
 func (s *identityService) CreateIdentity(ctx context.Context, req *requests.CreateIdentityRequest, hashedPassword string) (*responses.IdentityResponse, error) {
 	log := s.logger.WithContext(ctx)
-	sanitizedRole := strings.ReplaceAll(req.Role, "\n", "")
+	sanitizedRole := strings.ReplaceAll(string(req.Role), "\n", "")
 	sanitizedRole = strings.ReplaceAll(sanitizedRole, "\r", "")
 	log.Info("Creating new identity", "email", req.Email, "role", sanitizedRole)
 
